a891c00036b66ab7902b56ce25715fec4c008fba
Stefan Op de Beek <ste7an@gmail.com>
2024-08-23 08:16:46 +0200

    initial commit

---
+++ HID_Keyboard_new/APP/hidkbd_main.c
@@ -0,0 +1,135 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2020/08/06
+ * Description        : 蓝牙键盘应用主函数及任务系统初始化
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+/******************************************************************************/
+/* 头文件包含 */
+#include "CONFIG.h"
+#include "HAL.h"
+#include "hiddev.h"
+#include "hidkbd.h"
+
+/*********************************************************************
+ * GLOBAL TYPEDEFS
+ */
+__attribute__((aligned(4))) uint32_t MEM_BUF[BLE_MEMHEAP_SIZE / 4];
+
+#if(defined(BLE_MAC)) && (BLE_MAC == TRUE)
+const uint8_t MacAddr[6] = {0x84, 0xC2, 0xE4, 0x03, 0x02, 0x02};
+#endif
+
+/*********************************************************************
+ * @fn      Main_Circulation
+ *
+ * @brief   主循环
+ *
+ * @return  none
+ */
+__HIGH_CODE
+__attribute__((noinline))
+void Main_Circulation()
+{
+    while(1)
+    {
+        TMOS_SystemProcess();
+    }
+}
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   主函数
+ *
+ * @return  none
+ */
+
+
+int main(void)
+{
+#if(defined(DCDC_ENABLE)) && (DCDC_ENABLE == TRUE)
+    PWR_DCDCCfg(ENABLE);
+#endif
+    SetSysClock(CLK_SOURCE_PLL_60MHz);
+#if(defined(HAL_SLEEP)) && (HAL_SLEEP == TRUE)
+    GPIOA_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
+    GPIOB_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
+#endif
+
+GPIOB_ModeCfg(BUTTON_PIN, GPIO_ModeIN_PU);
+GPIOB_ITModeCfg(BUTTON_PIN, GPIO_ITMode_FallEdge);
+GPIOB_ClearITFlagBit(BUTTON_PIN);
+PFIC_EnableIRQ(GPIO_B_IRQn);
+
+
+GPIOB_ModeCfg(SELECT_PIN, GPIO_ModeIN_PU);
+GPIOB_ITModeCfg(SELECT_PIN, GPIO_ITMode_FallEdge);
+GPIOB_ClearITFlagBit(SELECT_PIN);
+PFIC_EnableIRQ(GPIO_B_IRQn);
+
+
+GPIOB_SetBits(LED_PIN);
+GPIOB_ModeCfg(LED_PIN, GPIO_ModeOut_PP_5mA);
+
+GPIOA_SetBits(LED_FORWARD_PIN);
+GPIOA_ModeCfg(LED_FORWARD_PIN, GPIO_ModeOut_PP_5mA);
+
+GPIOA_SetBits(LED_BACKWARD_PIN);
+GPIOA_ModeCfg(LED_BACKWARD_PIN, GPIO_ModeOut_PP_5mA);
+
+GPIOA_SetBits(LED_MIDDLE_LEFT_PIN);
+GPIOA_ModeCfg(LED_MIDDLE_LEFT_PIN, GPIO_ModeOut_PP_5mA);
+GPIOA_SetBits(LED_MIDDLE_RIGHT_PIN);
+GPIOA_ModeCfg(LED_MIDDLE_RIGHT_PIN, GPIO_ModeOut_PP_5mA);
+
+unsigned int leds[]={LED_PIN,LED_BACKWARD_PIN,LED_MIDDLE_LEFT_PIN,LED_MIDDLE_RIGHT_PIN,LED_FORWARD_PIN};
+for (int j=0; j<3; j++) {
+	for (int i=0; i<5; i++) {
+
+		if (i==0) {
+			GPIOB_ResetBits(leds[i]);
+			DelayMs(100);
+			GPIOB_SetBits(leds[i]);
+			//DelayMs(50);
+
+		} else {
+			GPIOA_ResetBits(leds[i]);
+			DelayMs(100);
+			GPIOA_SetBits(leds[i]);
+			//DelayMs(50);
+		}
+	}
+}
+
+DelayMs(500);
+GPIOA_SetBits(LED_FORWARD_PIN);
+GPIOA_ResetBits(LED_BACKWARD_PIN);
+
+TMR0_TimerInit(DEBOUNCE_TICKS);
+TMR0_ITCfg(ENABLE, TMR0_3_IT_CYC_END);
+PFIC_EnableIRQ(TMR0_IRQn);
+
+#define DEBUG 1
+#ifdef DEBUG
+    GPIOA_SetBits(bTXD1);
+    GPIOA_ModeCfg(bTXD1, GPIO_ModeOut_PP_5mA);
+    UART1_DefInit();
+#endif
+    PRINT("%s\n", VER_LIB);
+    PRINT("hallo van de uart");
+    CH58X_BLEInit();
+    HAL_Init();
+    GAPRole_PeripheralInit();
+    HidDev_Init();
+    HidEmu_Init();
+    Main_Circulation();
+}
+
+/******************************** endfile @ main ******************************/
---
+++ HID_Keyboard_new/APP/include/hidkbd.h
@@ -0,0 +1,90 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : hidkbd.h
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2018/12/10
+ * Description        :
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+#ifndef HIDKBD_H
+#define HIDKBD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************************************************************
+ * INCLUDES
+ */
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+
+
+
+// Task Events
+#define START_DEVICE_EVT          0x0001
+#define START_REPORT_EVT          0x0002
+#define START_PARAM_UPDATE_EVT    0x0004
+#define START_PHY_UPDATE_EVT      0x0008
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * FUNCTIONS
+ */
+
+#define MODE_FORWARD 0
+#define MODE_BACKWARD 1
+
+#define BUTTON_PIN GPIO_Pin_15  // PB15
+#define SELECT_PIN GPIO_Pin_4   //PB4
+
+#define LED_PIN				GPIO_Pin_7      // PB7
+#define LED_FORWARD_PIN		GPIO_Pin_15 //PA15
+#define LED_BACKWARD_PIN	GPIO_Pin_8 // PA8
+
+#define LED_MIDDLE_LEFT_PIN     GPIO_Pin_13 // PA13
+#define LED_MIDDLE_RIGHT_PIN    GPIO_Pin_14 // PA13
+
+		 // On clignote à 2Hz
+ #define DEBOUNCE_TIME_MS  50  // Total debounce time in milliseconds
+ #define CHECKS_COUNT   10  // Number of checks within the debounce period
+
+ #define DEBOUNCE_TICKS    ((DEBOUNCE_TIME_MS / CHECKS_COUNT) * (FREQ_SYS / 1000))  // Ticks per check interval
+
+
+#define HID_KEY_ARROW_RIGHT 0x4F
+#define HID_KEY_ARROW_LEFT 0x50
+#define HID_KEY_ARROW_DOWN 0x51
+#define HID_KEY_ARROW_UP 0x52
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+
+/*
+ * Task Initialization for the BLE Application
+ */
+extern void HidEmu_Init(void);
+
+/*
+ * Task Event Processor for the BLE Application
+ */
+extern uint16_t HidEmu_ProcessEvent(uint8_t task_id, uint16_t events);
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
